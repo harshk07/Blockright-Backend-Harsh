32598558a5a2bfc4081f521c4c943ca2f4faf11a
Krishna <krishnacool781@gmail.com>
2024-01-30 18:03:28 +0530

    Fixed Order api

--- route/orderRoute.py
+++ route/orderRoute.py
@@ -1,34 +1,44 @@
 from fastapi import APIRouter
 from schema.orderSchema import post_orderEcommerce, get_allOrder, patch_orderEcommerce
 from model.order import OrderModel
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
 
 order = APIRouter()
 
-@order.post("/order/ecommerce/", tags=['order'], summary="Allow User to Puchase Ecommerce")
+
+@order.post(
+    "/order/ecommerce/", tags=["order"], summary="Allow User to Purchase Ecommerce"
+)
 def read_root(data: OrderModel):
     """
-    ### Allow user to puchase ecommerce
-    [Documentation Here](https://docs.google.com/document/d/1HKzgolugj8g4oTkMoWPN3gDMprcB9geV7EDAL7MUBOA/edit#heading=h.4fpya9hkk4bs) 
+    ### Allow user to purchase ecommerce
+    [Documentation Here](https://docs.google.com/document/d/1HKzgolugj8g4oTkMoWPN3gDMprcB9geV7EDAL7MUBOA/edit#heading=h.4fpya9hkk4bs)
     """
     response = post_orderEcommerce(data)
     return {"response": response}
 
-@order.get("/order/get/{adminId}", tags=['order'], summary="Allow Admin to Get Order")
+
+@order.get("/order/get/{adminId}", tags=["order"], summary="Allow Admin to Get Order")
 def read_root(adminId):
     """
-    ### Allow Admin to Get Order 
+    ### Allow Admin to Get Order
     ### Condition: \n
     Payment should be done \n
-    [Documentation Here](https://docs.google.com/document/d/1HKzgolugj8g4oTkMoWPN3gDMprcB9geV7EDAL7MUBOA/edit#heading=h.4fpya9hkk4bs) 
+    [Documentation Here](https://docs.google.com/document/d/1HKzgolugj8g4oTkMoWPN3gDMprcB9geV7EDAL7MUBOA/edit#heading=h.4fpya9hkk4bs)
     """
     response = get_allOrder(adminId)
     return {"response": response}
 
-@order.patch("/order/patch_ecommerce/", tags=['order'], summary="Allow Admin to Update Order")
+
+@order.patch(
+    "/order/patch_ecommerce/", tags=["order"], summary="Allow Admin to Update Order"
+)
 def read_root(adminId, orderId, data):
     """
     ### Allow Admin to Update Order - orderStatus and orderAmount
-    [Documentation Here](https://docs.google.com/document/d/1HKzgolugj8g4oTkMoWPN3gDMprcB9geV7EDAL7MUBOA/edit#heading=h.4fpya9hkk4bs) 
+    [Documentation Here](https://docs.google.com/document/d/1HKzgolugj8g4oTkMoWPN3gDMprcB9geV7EDAL7MUBOA/edit#heading=h.4fpya9hkk4bs)
     """
-    response = patch_orderEcommerce(adminId,orderId,data)
+    response = patch_orderEcommerce(adminId, orderId, data)
     return {"response": response}
--- schema/orderSchema.py
+++ schema/orderSchema.py
@@ -2,53 +2,51 @@ from bson.objectid import ObjectId
 import json
 from datetime import datetime
 import secrets
+from model.order import *
+from config.db import *
+
 
 def post_orderEcommerce(data):
-    from config.db import order, user, product
-    data = dict(data)
-    
-    #Check User ID 
-    if user.find_one({"_id":ObjectId(data["userId"])}):
-        
-        #Check length of mobile number to be 10 digits
-        if(len(str(data["userMobile"]))==10 ):
-        
-        #Calculations
-        #TODO
-        # prod=product.find({})
-        # for item1 in prod:
-        #     for item2 in range(0:(data["product"]).len()):
-        #         if item1["images"]==item2["product"]["productImage"]:
-        #             c=prod["price"]*item2["product"]["quantity"]
-        #             d=c*prod["discount"]
-        #             sum=sum+(c-d)
-        
+    data_dict = dict(data)
 
-            order.insert_one(data)
+    if len(str(data_dict["userMobile"])) == 10:
+        data_dict.update(
+            {
+                "createdAt": datetime.now(),
+                "orderStatus": "Pending",
+                "paymentStatus": "Pending",
+                "paymentId": None,
+            }
+        )
 
-            order.update_one({"_id": data["_id"]}, {"$set": {"createdAt": datetime.now(),
-                                                    "orderAmount": bool(False),
-                                                    "products.orderStatus": str("Pending"),
-                                                    "paymentStatus": str("Pending"),
-                                                    "paymentId": None
-                                                    }})
-            return "Order Pending, Proceed for Payment"
-        else:
-            return "Please enter 10 digit mobile number"
+        # Rename the key to avoid conflicts
+        data_dict["orderedProducts"] = [
+            {
+                "quantity": product_data.quantity,
+                "color": product_data.color,
+                "size": product_data.size,
+                "verificationId": product_data.verificationId,
+            }
+            for product_data in data_dict.pop("products")
+        ]
+
+        order.insert_one(data_dict)
+
+        return "Order Pending, Proceed for Payment"
     else:
-        return "Invalid User Id"
-    
+        return "Please enter a 10-digit mobile number"
+
+
 def get_allOrder(id):
     from config.db import order, admin
+
     lst = []
     orderData = order.find({})
-    
+
     # Check if Admin is valid or not
     admin_data = admin.find_one({"_id": ObjectId(id)})
     if admin_data:
-        
         for item in orderData:
-                
             # Check if payment is done
             if item["paymentStatus"] != "Pending":
                 item["_id"] = str(item["_id"])
@@ -56,29 +54,27 @@ def get_allOrder(id):
         return lst
     else:
         return "Invalid Admin ID"
-    
+
+
 def patch_orderEcommerce(idA, idO, data):
     from config.db import order, admin
+
     data = json.loads(data)
 
     # Check if Admin is valid or not
     admin_data = admin.find_one({"_id": ObjectId(idA)})
     if admin_data:
-
         # Check if Order is there or not
         document = order.find_one({"_id": ObjectId(idO)})
         if document:
-
             # Only certain values can be patched
             if "products.orderStatus" in data or "orderAmount" in data:
                 order.update_one({"_id": ObjectId(idO)}, {"$set": data})
                 return "Order Detail Updated Successfully"
-            
+
             else:
                 return "This field is not Patchable"
         else:
             return "Invalid Order ID"
     else:
         return "Invalid Admin Id"
-
-    
