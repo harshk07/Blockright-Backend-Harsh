6393dcaa85d38b16de3115458d673c656075c2d0
HarshK07 <khapreharsh07@gmail.com>
2024-02-12 12:40:55 +0530

    Changes to be committed: 	modified:   model/payment.py 	modified:   schema/orderSchema.py 	modified:   schema/paymentSchema.py

--- schema/orderSchema.py
+++ schema/orderSchema.py
@@ -35,13 +35,71 @@ def post_orderEcommerce(data):
             for product_data in data_dict.pop("products")
         ]
 
-        order.insert_one(data_dict)
+        result = order.insert_one(data_dict)
+        print(result)
+
+        order_id = str(result.inserted_id)
 
         total_amount = sum(item["totalAmount"] for item in data_dict["orderedProducts"])
 
-        return f"Order Pending, Proceed for Payment. Total Amount: {total_amount}"
+        message = f"Order Pending, Proceed for Payment. Total Amount: {total_amount}"
+        return {"order_id": order_id, "Response": message}
+
     else:
         return "Please enter a 10-digit mobile number"
+
+
+# def post_orderEcommerce(data):
+#     data_dict = dict(data)
+
+#     if len(str(data_dict["userMobile"])) == 10:
+#         ordered_products = data_dict.pop("products")
+#         total_amount = 0
+
+#         for product_data in ordered_products:
+#             product_id = product_data["productId"]
+#             ordered_quantity = product_data["quantity"]
+
+#             product = Product.find_one({"_id": ObjectId(product_id)})
+#             if product:
+#                 available_quantity = Product.get("availableQuantity", 0)
+#                 if ordered_quantity <= available_quantity:
+#                     total_amount += float(product_data["price"]) * ordered_quantity
+
+#                     # Update available quantity
+#                     new_available_quantity = available_quantity - ordered_quantity
+#                     Product.update_one(
+#                         {"_id": ObjectId(product_id)},
+#                         {"$set": {"availableQuantity": new_available_quantity}},
+#                     )
+
+#                     # Update ordered product details
+#                     product_data["totalAmount"] = (
+#                         float(product_data["price"]) * ordered_quantity
+#                     )
+#                 else:
+#                     return f"Insufficient quantity available for product {product_data['merchTitle']}"
+
+#             else:
+#                 return f"Product with ID {product_id} not found"
+
+#         # Add additional order details
+#         data_dict.update(
+#             {
+#                 "createdAt": datetime.now(),
+#                 "orderStatus": "Pending",
+#                 "paymentStatus": "Pending",
+#                 "paymentId": None,
+#                 "orderedProducts": ordered_products,
+#             }
+#         )
+
+#         # Insert order into the database
+#         order.insert_one(data_dict)
+
+#         return f"Order Pending, Proceed for Payment. Total Amount: {total_amount}"
+#     else:
+#         return "Please enter a 10-digit mobile number"
 
 
 def get_allOrder(id):
