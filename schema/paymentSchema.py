0acede5b7e261c2c45c0129e77111eeb3492d61c
HarshK07 <khapreharsh07@gmail.com>
2024-02-12 14:02:12 +0530

    Implemented transaction API. status changed from completed to confirmed of payment status

--- schema/paymentSchema.py
+++ schema/paymentSchema.py
@@ -1,4 +1,4 @@
-from bson.objectid import ObjectId
+from bson import ObjectId
 import json
 from datetime import datetime
 import secrets
@@ -11,10 +11,10 @@ def post_userPayment(data):
     print(thatOrder)
 
     if thatOrder:
-        if thatOrder["paymentStatus"] is not "completed":
+        if thatOrder["paymentStatus"] != "confirmed":
             order.update_one(
                 {"_id": ObjectId(data_dict["orderId"])},
-                {"$set": {"paymentStatus": "completed"}},
+                {"$set": {"paymentStatus": "confirmed"}},
             )
 
             for eachProduct in thatOrder["orderedProducts"]:
@@ -45,7 +45,7 @@ def post_userPayment(data):
             print(response)
             return response
         else:
-            return "Payment status is already set to completed"
+            return "Payment status is already set to Confirmed"
 
     else:
         message = "Order does not found with orderId: " + str(data_dict["orderId"])
@@ -54,3 +54,48 @@ def post_userPayment(data):
 
 def patch_adminPayment(idA, idP, data):
     from config.db import payment, admin, order
+
+
+def get_transactionDetails(walletAddress):
+    allOrders = order.find()
+    transactionList = []
+    for eachOrder in allOrders:
+        if eachOrder["paymentStatus"] == "confirmed":
+            for eachOrderedProduct in eachOrder["orderedProducts"]:
+                getProductId = eachOrderedProduct["productId"]
+                print("product Id: " + str(getProductId))
+
+                # check if this productId matches with the product document
+                # ye product id ko seach karo product collection me aur agar isak walletAddres match kar jay given wallet address se.
+                # to selected details ko get karke transaction page me show kar do.
+
+                thatProduct = product.find_one(
+                    {"walletAddress": walletAddress, "_id": ObjectId(getProductId)}
+                )
+                print(thatProduct)
+                if product:
+                    for eachItem in eachOrder["orderedProducts"]:
+                        transaction_dict = {}
+                        title = eachItem["productTitle"]
+                        category = thatProduct["category"]
+                        quantity = eachItem["quantity"]
+                        address = eachOrder["address"]
+                        country = eachOrder["country"]
+                        time = eachOrder["createdAt"]
+
+                        transaction_dict.update(
+                            {
+                                "title": title,
+                                "category": category,
+                                "quantity": quantity,
+                                "address": address,
+                                "country": country,
+                                "time": time,
+                            }
+                        )
+
+                    print(transaction_dict)
+
+                    transactionList.append(transaction_dict)
+
+    return transactionList
